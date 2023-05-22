8fde3cddb37d0e491c999fd504b24766700048b6
“lipesshw” <lipebroken@gmail.com>
2023-05-22 18:38:30 -0300

    add mobile responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import todoLogo from "../../assets/todoLogo.svg";
 import { AiOutlinePlusCircle, } from "react-icons/ai";
 import styles from "./header.module.css";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState, useEffect  } from "react";
 import { toast } from "react-toastify";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -12,6 +12,25 @@ interface Props {
 
 export function Header({ onAddTask }: Props) {
   const [title, setTitle] = useState("");
+  const [hideContent, setHideContent] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setHideContent(true);
+      } else {
+        setHideContent(false);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
 
   function handleSubmit(event: FormEvent) {
     event.preventDefault();
@@ -32,20 +51,26 @@ export function Header({ onAddTask }: Props) {
 
   return (
     <header className={styles.header}>
-      <img src={todoLogo} />
-      <ToastContainer position="top-right" autoClose={3000} hideProgressBar={true} />
-      <form onSubmit={handleSubmit} className={styles.newTaskForm}>
-        <input
-          placeholder="Adicione uma nova tarefa"
-          type="text"
-          value={title}
-          onChange={onChangeTitle}
-        />
-        <button type="submit">
-          Adicionar
-          <AiOutlinePlusCircle size={20} />
-        </button>
-      </form>
-    </header>
+    <img src={todoLogo} />
+    <ToastContainer position="top-right" autoClose={3000} hideProgressBar={true} />
+    <form onSubmit={handleSubmit} className={`${styles.newTaskForm} ${hideContent ? "hide-content" : "a"}`}>
+      <input
+        placeholder="Adicione uma nova tarefa"
+        type="text"
+        value={title}
+        onChange={onChangeTitle}
+      />
+      <button type="submit">
+        {hideContent ? <AiOutlinePlusCircle size={20} /> : (
+          <>
+            Adicionar
+         <span>
+            <AiOutlinePlusCircle size={20} />
+          </span>
+        </>
+      )}
+    </button>
+  </form>
+</header>
   );
 }
